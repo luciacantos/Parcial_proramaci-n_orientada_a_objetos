2edc62f092f8d9cc010dea6e6315865329003d49
luciacantos <luciacantos22@gmail.com>
2023-03-24 11:03:25 +0100

    métodos accesores

---
+++ ejercicio_01/ejercicio_01.py
@@ -0,0 +1,18 @@
+class Libro:
+    def __init__(self,título,autor,editorial,páginas,año,):
+        self.título = título
+        self.autor = autor
+        self.editorial = editorial
+        self.páginas = páginas
+        self.año = año
+
+    def getTítulo(self):
+        return self.tíyulo
+    def getAutor(self):
+        return self.autor
+    def getEditorial(/self):
+        return self.editorial
+    def getPáginas(self):
+        return self.páginas
+    def getAño(self):
+        return self.año
